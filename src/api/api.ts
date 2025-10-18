2f23b1a5dd9811675e26fb2dc6bdafcf8e948b8f
Mohamed Salem El Eze <mohamedsalemeleze@gmail.com>
2025-10-18 18:51:22 +0000

    update nginx

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 // api/api.ts
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const getInvoiceById = async (invoiceId: string) => {
     try {
